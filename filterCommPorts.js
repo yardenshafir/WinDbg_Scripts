2be3dccd72b895c0f638a2ef41a192677a4071e3
yardenshafir <yardenshafir2@gmail.com>
2023-01-16 14:43:33 -0500

    Tabs to spaces

--- filterCommPorts.js
+++ filterCommPorts.js
@@ -3,10 +3,10 @@
 function initializeScript()
 {
     return [new host.functionAlias(GetHandlesToDevice, "DeviceFileHandles"),
-			new host.functionAlias(GetQueuedIrpsForPort, "IrpsForPort"),
-			new host.functionAlias(RegisteredFilterDrivers, "RegisteredFilters"),
-			new host.functionAlias(EnumPortsForFilter, "EnumPortsForFilter"),
-	        new host.apiVersionSupport(1, 6)];
+            new host.functionAlias(GetQueuedIrpsForPort, "IrpsForPort"),
+            new host.functionAlias(RegisteredFilterDrivers, "RegisteredFilters"),
+            new host.functionAlias(EnumPortsForFilter, "EnumPortsForFilter"),
+            new host.apiVersionSupport(1, 6)];
 }
 
 function GetHandlesToDevice(Device)
@@ -31,13 +31,13 @@ function GetHandlesToDevice(Device)
  
                     let fileObj = handle.Object.ObjectType;
                     if (fileObj === "File") {
-						if (host.parseInt64(handle.Object.UnderlyingObject.DeviceObject.address, 16).compareTo(Device) == 0)
+                        if (host.parseInt64(handle.Object.UnderlyingObject.DeviceObject.address, 16).compareTo(Device) == 0)
                         {
-							let fscontext2 = handle.Object.UnderlyingObject.FsContext2.address;
-							let fltCcbType = host.getModuleType("FltMgr", "_FLT_CCB");
-							let port = host.createTypedObject(fscontext2, fltCcbType).Data.Port.Port;
-							let portObjHeader = host.createTypedObject(port.ServerPort.address.subtract(objHeaderOffset), objHeaderType);
-							dbgOutput("\tProcess ", process.Name, " has handle ", handle.Handle, " to port ", portObjHeader.ObjectName, "\n");
+                            let fscontext2 = handle.Object.UnderlyingObject.FsContext2.address;
+                            let fltCcbType = host.getModuleType("FltMgr", "_FLT_CCB");
+                            let port = host.createTypedObject(fscontext2, fltCcbType).Data.Port.Port;
+                            let portObjHeader = host.createTypedObject(port.ServerPort.address.subtract(objHeaderOffset), objHeaderType);
+                            dbgOutput("\tProcess ", process.Name, " has handle ", handle.Handle, " to port ", portObjHeader.ObjectName, "\n");
 							
                         }
                     }
@@ -68,59 +68,56 @@ function GetQueuedIrpsForPort(Port)
 	let waiterlist = host.namespace.Debugger.Utility.Collections.FromListEntry(v_port.MsgQ.WaiterQ.mList, "nt!_IRP", "Tail.Overlay.ListEntry");
 	
 	for (let entry of waiterlist) {
-		dbgOutput("\tIRP address: ", entry.address, " by thread ", entry.CurrentThread.Cid.UniqueThread.address,"\n");
+            dbgOutput("\tIRP address: ", entry.address, " by thread ", entry.CurrentThread.Cid.UniqueThread.address,"\n");
 	}
 }
 
 function RegisteredFilterDrivers()
 {
-	let dbgOutput = host.diagnostics.debugLog;
-	let fltglobals = host.getModuleSymbolAddress("fltmgr", "FltGlobals");
-	let typedFltGlobals = host.createTypedObject(fltglobals, "fltmgr", "_GLOBALS");
-	
-	dbgOutput("Flt Globals: ", typedFltGlobals.address, "\n");
-	
-	let flt_frames = host.namespace.Debugger.Utility.Collections.FromListEntry(typedFltGlobals.FrameList.rList, "fltmgr!_FLTP_FRAME", "Links");
-	
-	for (let flt_frame of flt_frames) {
-		dbgOutput("Frame ", flt_frame.FrameID, "\n");
-		
-		let filters = host.namespace.Debugger.Utility.Collections.FromListEntry(flt_frame.RegisteredFilters.rList, "fltmgr!_FLT_FILTER", "Base.PrimaryLink");
-		
-		for (let filter of filters) {
-			dbgOutput("\tFilter ", filter.Name, " : ", filter.address, "\n");
-			
-			let instances = host.namespace.Debugger.Utility.Collections.FromListEntry(filter.InstanceList.rList, "fltmgr!_FLT_INSTANCE", "FilterLink");
-			for (let instance of instances) {
-				dbgOutput("\t\tInstance: ", instance.address, ", Name: ", instance.Name, ", Altitude: ", instance.Altitude, "\n");
-			}
+    let dbgOutput = host.diagnostics.debugLog;
+    let fltglobals = host.getModuleSymbolAddress("fltmgr", "FltGlobals");
+    let typedFltGlobals = host.createTypedObject(fltglobals, "fltmgr", "_GLOBALS");
 
-		}
-	}
+    dbgOutput("Flt Globals: ", typedFltGlobals.address, "\n");
+
+    let flt_frames = host.namespace.Debugger.Utility.Collections.FromListEntry(typedFltGlobals.FrameList.rList, "fltmgr!_FLTP_FRAME", "Links");
+
+    for (let flt_frame of flt_frames) {
+        dbgOutput("Frame ", flt_frame.FrameID, "\n");
+        let filters = host.namespace.Debugger.Utility.Collections.FromListEntry(flt_frame.RegisteredFilters.rList, "fltmgr!_FLT_FILTER", "Base.PrimaryLink");
+	
+        for (let filter of filters) {
+            dbgOutput("\tFilter ", filter.Name, " : ", filter.address, "\n");
+		
+            let instances = host.namespace.Debugger.Utility.Collections.FromListEntry(filter.InstanceList.rList, "fltmgr!_FLT_INSTANCE", "FilterLink");
+            for (let instance of instances) {
+                dbgOutput("\t\tInstance: ", instance.address, ", Name: ", instance.Name, ", Altitude: ", instance.Altitude, "\n");
+            }
+        }
+    }
 }
 
 function EnumPortsForFilter(Filter)
 {
-	let dbgOutput = host.diagnostics.debugLog;
-	let v_filter = host.createTypedObject(Filter, host.getModuleType("FltMgr", "_FLT_FILTER"));
-	dbgOutput("Filter: ", v_filter.address, "\n");
+    let dbgOutput = host.diagnostics.debugLog;
+    let v_filter = host.createTypedObject(Filter, host.getModuleType("FltMgr", "_FLT_FILTER"));
+    dbgOutput("Filter: ", v_filter.address, "\n");
 
+    if (v_filter.PortList.mCount != 0) {
+        let ports = host.namespace.Debugger.Utility.Collections.FromListEntry(v_filter.PortList.mList, "fltmgr!_FLT_PORT_OBJECT", "FilterLink");
 
-	if (v_filter.PortList.mCount != 0) {
-		let ports = host.namespace.Debugger.Utility.Collections.FromListEntry(v_filter.PortList.mList, "fltmgr!_FLT_PORT_OBJECT", "FilterLink");
-
-		for (let port of ports) {
-			if (port != 0) {
-				dbgOutput("Port: ", port.address, "\n");
-				dbgOutput("\tServerPort: ", port.ServerPort.address, "\n");
-				dbgOutput("\t\tConnectNotify: ", port.ServerPort.ConnectNotify.address, "\n");
-				dbgOutput("\t\tDisconnectNotify: ", port.ServerPort.DisconnectNotify.address, "\n");
-				dbgOutput("\t\tMessageNotify: ", port.ServerPort.MessageNotify.address, "\n");
-				dbgOutput("\t\tNumberOfConnections: ", port.ServerPort.NumberOfConnections, "\n");
-				dbgOutput("\t\tMaxConnections: ", port.ServerPort.MaxConnections, "\n");
-				dbgOutput("\tMsgQ: ", port.MsgQ.address, "\n");
-				dbgOutput("\tDisconnected: ", port.Disconnected, "\n");
-			}
-		}
-	}	
+        for (let port of ports) {
+            if (port != 0) {
+                dbgOutput("Port: ", port.address, "\n");
+                dbgOutput("\tServerPort: ", port.ServerPort.address, "\n");
+                dbgOutput("\t\tConnectNotify: ", port.ServerPort.ConnectNotify.address, "\n");
+                dbgOutput("\t\tDisconnectNotify: ", port.ServerPort.DisconnectNotify.address, "\n");
+                dbgOutput("\t\tMessageNotify: ", port.ServerPort.MessageNotify.address, "\n");
+                dbgOutput("\t\tNumberOfConnections: ", port.ServerPort.NumberOfConnections, "\n");
+                dbgOutput("\t\tMaxConnections: ", port.ServerPort.MaxConnections, "\n");
+                dbgOutput("\tMsgQ: ", port.MsgQ.address, "\n");
+                dbgOutput("\tDisconnected: ", port.Disconnected, "\n");
+	    }
+	}
+    }	
 }
